4aaf2260eafe5b2b7ade1f8e79b64caa06ff80d9
ryo.shiseki <ryo_ssk0629@yahoo.co.jp>
2020-05-22 19:28:43 +0900

    Modify Index/NewServlet

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,9 +1,9 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,26 +33,13 @@ public class NewServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 EntityManager em = DBUtil.createEntityManager();
-	        em.getTransaction().begin();
+		 request.setAttribute("_token", request.getSession().getId());
 
-	        // Taskのインスタンスを生成
-	        Task m = new Task();
+	        // おまじないとしてのインスタンスを生成
+	        request.setAttribute("task", new Task());
 
-	        String content = "hello";
-	        m.setContent(content);
-
-	        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
-	        m.setCreated_at(currentTime);
-	        m.setUpdated_at(currentTime);
-
-	        // データベースに保存
-	        em.persist(m);
-	        em.getTransaction().commit();
-
-	        // 自動採番されたIDの値を表示
-	        response.getWriter().append(Integer.valueOf(m.getId()).toString());
-
-	        em.close();
+	        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+	        rd.forward(request, response);
 	     }
 
 }
